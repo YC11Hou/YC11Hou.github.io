8f74dfd490f2509a248b9ca7f1ca9e4667c6c618
YC11Hou <yuchenhou287@outlook.com>
2026-09-04 04:46:18 +0000

    Deploying to gh-pages from @ YC11Hou/YC11Hou.github.io@3a7478a328156deb79be5ab870e91b5fa39fcb27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,6 +92,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/yuchen-hou-115025355", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=4oFdrl8AAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
