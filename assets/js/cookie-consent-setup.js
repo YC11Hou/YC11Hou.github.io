caa6a19da7f236f9443689659a6c531adf5d61a0
YC11Hou <yuchenhou287@outlook.com>
2026-09-04 16:04:14 +0000

    Deploying to gh-pages from @ YC11Hou/YC11Hou.github.io@8e9ec88c6c1805188af9056ff264397f2c13a6ca 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://yc11hou.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://yuchenhou.me/#contact">contact us</a>.'
               }
             ]
           }
